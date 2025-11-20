cb9ad6f3ca21927809291bf3cd2c927e337a5d4f
Believer in christ's life Bible Church <bclbconline@gmail.com>
2025-11-20 10:05:27 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useMemo, type ReactNode } from 'react';
+import React, { type ReactNode } from 'react';
 import { FirebaseProvider, initializeFirebase } from '.';
 
 // This provider is responsible for initializing Firebase on the client side.
@@ -9,7 +9,7 @@ import { FirebaseProvider, initializeFirebase } from '.';
 // It ensures that Firebase is initialized only once and that the same instance
 // is used throughout the application.
 export function FirebaseClientProvider({ children }: { children: ReactNode }) {
-  const { firebaseApp, auth, firestore } = useMemo(
+  const { firebaseApp, auth, firestore } = React.useMemo(
     () => initializeFirebase(),
     []
   );
